b3a7409f4810660be2841c470167b36654841217
Xenthio <thegooster88@gmail.com>
2025-08-31 09:15:33 +1000

    label focus vgui theme

---
+++ RTXLauncher.Avalonia/Utilities/VGUIThemeUtils.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System;
+using System.Globalization;
+
+namespace RTXLauncher.Avalonia.Utilities;
+
+public class BoolToColorConverter : IValueConverter
+{
+	public static readonly BoolToColorConverter Instance = new();
+
+	public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		if (value is bool isFocused)
+		{
+			// Get the color resources from the application
+			if (Application.Current?.TryFindResource("VguiTextActiveBrush", out var activeBrush) == true && activeBrush is IBrush active &&
+				Application.Current?.TryFindResource("VguiTextLabelBrush", out var labelBrush) == true && labelBrush is IBrush label)
+			{
+				return isFocused ? active : label;
+			}
+		}
+
+		// Fallback to label brush
+		if (Application.Current?.TryFindResource("VguiTextLabelBrush", out var fallback) == true && fallback is IBrush fallbackBrush)
+		{
+			return fallbackBrush;
+		}
+
+		return null;
+	}
+
+	public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
